c66207efdc8faf2ec678a02dd6d6c66f3c4ccbd5
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-07-17 11:51:25 +0200

    Update webport

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello Keylane from Puppet Pipelines. Your Node.js application is working!\n');
-}).listen(801);
+}).listen(80);
 
 console.log('Server started');
